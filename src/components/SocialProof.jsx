bd30799bfce458ee15f5d5f7600bed9617a1f0bf
Lewis Sheridan <lewis@backplane.cloud>
2025-06-08 12:38:21 +0100

    css fixes for mobile

--- src/components/SocialProof.jsx
+++ src/components/SocialProof.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const SocialProof = () => {
   return (
     <section className=" dark:bg-crow-dark">
-      <div className="w-1/2 px-4 py-4 mx-auto text-center lg:py-4 lg:px-6">
+      <div className="md:w-1/2 px-4 py-4 mx-auto text-center lg:py-4 lg:px-6">
         <div className=" mb-8 lg:mb-16">
-          <h2 className="mb-4 text-xl md:text-4xl sm:text-xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Risk without visibility is risk accepted
+          <h2 className="mb-4 text-2xl md:text-4xl sm:text-xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            Risk without visibility is risk acceptedß
           </h2>
           <p className="text-gray-500 sm:text-xl dark:text-gray-400">
             CrowsNest empowers modern enterprises to maintain security assurance

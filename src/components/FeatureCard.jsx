7b06729bd169eec20db5a564d4ab04f130227899
Lewis Sheridan <lewis@backplane.cloud>
2025-06-09 18:13:29 +0200

    reverted to /www/ and redesigned featurecard

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -6,35 +6,29 @@ const FeatureCard = ({ icon, name, description }) => {
 
       {/* FeatureCard Card */}
 
-      <div className="border rounded-lg p-5 w-96">
-        <div className="flex flex-col">
-          <div className="flex flex-row">
-            <div className=" justify-center  mb-4   bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="36"
-                height="36"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="text-crow-primary dark:text-gray-100"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20" />
-                <path d="M2 12h20" />
-              </svg>
-            </div>
-            <div className="items-center flex mx-auto">
-              <h3 className="mb-2 text-lg text-crow-primary text-center font-bold dark:text-white h-12">
-                {name}
-              </h3>
-            </div>
+      <div className="border rounded-lg w-96">
+        <div className="flex flex-row">
+          <div className="w-32 p-5  bg-gray-100 justify-center   bg-primary-100 dark:bg-primary-900">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="text-crow-primary"
+            >
+              <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
+              <circle cx="12" cy="12" r="4" />
+            </svg>
           </div>
-
-          <div>
+          <div className="flex flex-col p-5">
+            <h3 className="mb-2 text-lg text-crow-primary text-center font-bold dark:text-white h-12">
+              {name}
+            </h3>
             <p className="text-gray-500 dark:text-gray-400 text-xs">
               {description}
             </p>

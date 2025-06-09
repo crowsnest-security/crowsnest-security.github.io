43183079fd785501e117166094b13b4f002dec48
Lewis Sheridan <lewis@backplane.cloud>
2025-06-09 18:19:29 +0200

    trying fix for gh pages css processing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./docs/**/*.{js,jsx,ts,tsx,md,mdx}",
     "./blog/**/*.{js,jsx,ts,tsx,md,mdx}",
     "./docusaurus.config.js",
+    "./*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
